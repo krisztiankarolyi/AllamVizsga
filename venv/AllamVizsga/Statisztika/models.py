0822612bf4bcd35edb575752e49b2d387bd264fa
krisztian_karolyi <xkiki2001@gmail.com>
2023-11-30 17:50:35 +0200

    stac teszt

--- venv/AllamVizsga/Statisztika/models.py
+++ venv/AllamVizsga/Statisztika/models.py
@@ -5,27 +5,31 @@ from matplotlib import pyplot as plt
 import numpy as np
 import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import kpss
 import base64
 import io
 
 class Stat:
     megye_nev = ""
+    adf = {};
+    kpss = {}
     atlag = szoras = varriancia = median = min = max = 0.0
     minDatum = maxDatum = ""
     adatok = []
     teszt_adatok = []
     teszt_idoszakok = []
     becslesek = []
-    mse = 0
-    rrmse = 0
+    mse = 0; rrmse = 0
     title = ""
     pacf_acf_Diagram  = ""
-
+    
 
 
     def __init__(self, megye_nev, adatok, idoPontok):
         self.megye_nev = megye_nev
         self.adatok = adatok
+
         self.atlag = round(np.mean(adatok), 2)
         self.szoras = round(np.std(adatok), 2)
         self.variancia = round(np.var(adatok), 2)
@@ -34,6 +38,8 @@ class Stat:
         self.max = np.max(adatok)
         self.minDatum = idoPontok[list.index(adatok, self.min)]
         self.maxDatum = idoPontok[list.index(adatok, self.max)]
+        self.adf = {}; self.kpss = {}
+        self.Stationarity()
     
 
     def setTesztAdatok(self, teszt_adatok: list):
@@ -47,8 +53,18 @@ class Stat:
         mse = self.MSE()
         mean_y = np.mean(self.teszt_adatok)
         return np.sqrt(mse) / mean_y
+    
+    def Stationarity(self):
+        adf_result = adfuller(self.adatok)
+        self.adf["adf_stat"] = adf_result[0]
+        self.adf["p_value"] = adf_result[1]
+        self.adf["critical_values"] = adf_result[4]
 
-        
+        kpss_result = kpss(self.adatok)
+        self.kpss["kpss_stat"] = kpss_result[0]
+        self.kpss["p_value"] = kpss_result[1]
+        self.kpss["critical_values"] = kpss_result[3]
+
     def AR(self, p: int, t:int):
         try:
             p = int(p)
